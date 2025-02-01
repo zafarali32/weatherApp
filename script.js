8f50728495d26dc13a925c361aca1cc6b3dd56ff
zafarali32 <zafaralisalar32@gmail.com>
2025-02-01 16:25:31 +0500

    main

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 const inputBox = document.querySelector('.input-box');
 const searchBtn = document.getElementById('searchBtn');
-const weather_img = document.querySelector('.weathere-img');
+const weather_img = document.querySelector('.weathere-image');
 const temperature = document.querySelector('.temperature');
-const discription = document.querySelector('.description');
+const description = document.querySelector('.description');
 const humidity = document.getElementById('humidity');
-const wind_speed = document.getElementById('wind-speed');
+const wind_speed = document.getElementById('wind_speed');
+const location_not_found = document.querySelector('.location-not-found');
+const weather_body = document.querySelector('.weather-body');
 
  async function checkWeather(city){
     const api_key = "a0905352758ba536120bfd2e61cd7f36";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch (`${url}`).then(response =>
-    response.json());
-    
+        response.json());
+
+
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        console.log("error");
+        return;
+    }
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
     temperature.innerHTML = `${Math.round (weather_data.main.temp - 273.15)}°C`;
-    discription.innerHTML = `${weather_data.weather[0].discription}`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}km/h`;
+
+    switch(weather_data.weather[0].main){
+        case 'clouds':
+        weather_img.src = "image/cloud.png";
+        break;
+        case 'Rain':
+        weather_img.src = "image/rain.png";
+        break;
+        case 'Mist':
+        weather_img.src = "image/mist.png";
+        break;
+        case 'Snow':
+        weather_img.src = "image/snow.png";
+        break;
+        case 'clear sky':
+        weather_img.src = "image/clear.png";
+        break;
+    }
 
     
     
